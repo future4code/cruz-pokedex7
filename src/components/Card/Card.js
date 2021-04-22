297328ea007d052be7ae19a60867e96aa80c060f
tandersonf92 <tandersonf@hotmail.com>
2021-04-22 11:29:03 -0300

    Esboco do Card feito e separado o arquivo do Styles.

---
+++ src/components/Card/Card.js
@@ -0,0 +1,37 @@
+
+import React from 'react'
+import { colors } from '../../constants/backColorsCards'
+import { ContainerPoke, ImageContainer, Number, BodyContainer, MainTypeContainer, PokemonName, Button, ButtonContainner, ContainerType } from './Styles'
+
+const color = colors
+
+
+const Card = (props) => {
+    const { name, type } = props
+    const colorContainer = color[props.type]
+
+    return (
+        <ContainerType >
+            <ContainerPoke>
+                <ImageContainer>
+                    <Number>
+                        {props.number}
+                    </Number>
+                </ImageContainer>
+                <BodyContainer >
+                    <MainTypeContainer style={{ backgroundColor: colorContainer }} >
+                        {type.charAt(0).toUpperCase() + type.slice(1)}
+                    </MainTypeContainer>
+                    <PokemonName>
+                        {name.charAt(0).toUpperCase() + name.slice(1)}
+                    </PokemonName>
+                </BodyContainer>
+                <ButtonContainner>
+                    <Button onClick={() => props.functionAddOrRemove()}>Adicionar</Button>
+                    <Button onClick={() => props.details()}>Detalhes</Button>
+                </ButtonContainner>
+            </ContainerPoke>
+        </ContainerType>
+    )
+}
+export default Card
