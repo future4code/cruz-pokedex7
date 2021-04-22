f7d7d478774e8723711cea855a4c4c4fea57170e
tandersonf92 <tandersonf@hotmail.com>
2021-04-22 11:55:20 -0300

    funcoes adicionadas, e funcao de sort funcionando para organizaçao

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -1,4 +1,4 @@
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
 import { goToPokedex, goToLastPage } from "../routes/coordinator";
 import axios from "axios";
@@ -14,81 +14,88 @@ const HomePage = () => {
 
 
 
-  
 
 
-    useEffect(() => {
-        getPokeInfo()
-    }, [])   
 
-     const [favoritePokes, setFavoritePokes] = useState([])
-    const [pokeData, setPokeData] = useState([])
-    const arrayPokes = []  
-      const getPokeInfo = async () => {
-        try {
-            const response = await axios.get(`https://pokeapi.co/api/v2/pokemon`)
-            for (let data of response.data.results) {
-                const response = await axios.get(data.url)
-                const dataOnePoke = response.data
-                arrayPokes.push(dataOnePoke)
-            }
-        } catch (error) {
-            console.log("Erro encontrado: ", error)
-        }
-        setPokeData(arrayPokes)
-    }  
+  useEffect(() => {
+    getPokeInfo()
+  }, [])
 
-      const addToFavorites = (pokemon) => {
-        let favorite = [...favoritePokes, pokemon]
-        setFavoritePokes(favorite)
-        const favorites = pokeData.filter((id) => pokemon !== id)
-        setPokeData(favorites)
-    }  
+  const [favoritePokes, setFavoritePokes] = useState([])
+  const [pokeData, setPokeData] = useState([])
+  const arrayPokes = []
+  const getPokeInfo = async () => {
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon`)
+      for (let data of response.data.results) {
+        const response = await axios.get(data.url)
+        const dataOnePoke = response.data
+        arrayPokes.push(dataOnePoke)
+      }
+    } catch (error) {
+      console.log("Erro encontrado: ", error)
+    }
+    setPokeData(arrayPokes)
+  }
 
-    const removeToFavorites = (pokemon) => {
-      
-      const newFavorites = favoritePokes.filter(id => pokemon !== id)
-        setFavoritePokes(newFavorites)
-        let homePokemons = [...pokeData, pokemon]
-        homePokemons.sort(function(a,b){
-            if(a.id > b.id){
-                return 1
-            }else {
-                return -1
-            }
-        })
-        setPokeData(homePokemons)
-    }   
-
-     const renderHomeCards = pokeData.map((pokemon) => {
-        return (
-            <Card
-                key={pokemon.id}
-                name={pokemon.name}
-                url={pokemon.url}
-                number={pokemon.id}
-                image={pokemon.sprites.front_default}
-                type={pokemon.types[0].type.name}
-                functionAddOrRemove={() => addToFavorites(pokemon)}
-                remove={() => removeToFavorites(pokemon)}
-            />
-        )
-    })   
-
-     const renderFavorites = favoritePokes.map((pokemon) => {
-        return (
-            <Card
-                key={pokemon.id}
-                name={pokemon.name}
-                url={pokemon.url}
-                number={pokemon.id}
-                image={pokemon.sprites.front_default}
-                type={pokemon.types[0].type.name}
-                add={() => addToFavorites(pokemon)}
-                remove={() => removeToFavorites(pokemon)}
-            />
-        )
+  const addToFavorites = (pokemon) => {
+    let favorite = [...favoritePokes, pokemon]
+    favorite.sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1
+      } else {
+        return -1
+      }
     })
+    setFavoritePokes(favorite)
+    const favorites = pokeData.filter((id) => pokemon !== id)
+    setPokeData(favorites)
+  }
+
+  const removeToFavorites = (pokemon) => {
+
+    const newFavorites = favoritePokes.filter(id => pokemon !== id)
+    setFavoritePokes(newFavorites)
+    let homePokemons = [...pokeData, pokemon]
+    homePokemons.sort(function (a, b) {
+      if (a.id > b.id) {
+        return 1
+      } else {
+        return -1
+      }
+    })
+    setPokeData(homePokemons)
+  }
+
+  const renderHomeCards = pokeData.map((pokemon) => {
+    return (
+      <Card
+        key={pokemon.id}
+        name={pokemon.name}
+        url={pokemon.url}
+        number={pokemon.id}
+        image={pokemon.sprites.front_default}
+        type={pokemon.types[0].type.name}
+        functionAddOrRemove={() => addToFavorites(pokemon)}
+        remove={() => removeToFavorites(pokemon)}
+      />
+    )
+  })
+
+  const renderFavorites = favoritePokes.map((pokemon) => {
+    return (
+      <Card
+        key={pokemon.id}
+        name={pokemon.name}
+        url={pokemon.url}
+        number={pokemon.id}
+        image={pokemon.sprites.front_default}
+        type={pokemon.types[0].type.name}
+        add={() => addToFavorites(pokemon)}
+        remove={() => removeToFavorites(pokemon)}
+      />
+    )
+  })
 
 
 
